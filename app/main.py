07e57cdab9711bbfe3f5e0a19c5f9d8fb4b0f85a
Resham-Patel <rudhraresham@gmail.com>
2026-03-19 18:20:25 +0530

    add

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from app.routes import auth_routes
-from app.database import Base, engine
+from app.database import Base, engine, SessionLocal
+from app.models.user_model import User
 
 app = FastAPI()
 
 # Create tables
 Base.metadata.create_all(bind=engine)
 
-# Include routes
-app.include_router(auth_routes.router)
+@app.get("/")
+def home():
+    return {"message": "DB Connected"}
+
+@app.get("/test-db")
+def test_db():
+    db = SessionLocal()
+    new_user = User(email="test@gmail.com", password="1234")
+    db.add(new_user)
+    db.commit()
+    db.close()
+    return {"message": "Inserted"}
